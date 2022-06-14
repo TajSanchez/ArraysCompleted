7662bba82af14d8ac8fc6cbb519d7a8b746f4b7e
Taj <tajmarie92@gmail.com>
2022-06-13 21:43:37 -0500

    Add project files.

---
+++ Arrays/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name01 = "Marie";
+            string name02 = "Monica";
+            string name03 = "Nick";
+            string name04 = "Noah";
+
+            string[] names = new string[4];
+            names[0] = "Marie";
+            names[1] = "Monica";
+            names[2] = "Nick";
+            names[3] = "Noah";
+
+          Console.WriteLine( names[2]);
+        }
+
+    }
+}
